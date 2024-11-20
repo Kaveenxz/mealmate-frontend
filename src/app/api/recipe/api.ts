c564b982b97dea177b78af856bb910e6e4e3fcbe
Kaveenxz <kaveenhansithx@gmail.com>
2024-11-20 11:02:24 +0530

    commit 3

--- src/app/api/recipe/api.ts
+++ src/app/api/recipe/api.ts
@@ -1,5 +1,5 @@
 export async function fetchRecipes() {
-  const API_KEY = ""; 
+  const API_KEY = "40733449f25848e48286f6f6e674083a"; 
   const response = await fetch(
     `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}`
   );
